7ea6f6ec4385286c451c381bfbf89a5415501d81
Clau Clouds <cacquist@meteo.uni-koeln.de>
2021-04-14 22:46:03 +0200

    attempts failed to remove vertical bars on 2dplots

--- quicklooks_wband_hourly.py
+++ quicklooks_wband_hourly.py
@@ -7,6 +7,11 @@ Created on Thu Feb  4 20:10:13 2021
 """
 
 # importing necessary libraries
+from matplotlib import rcParams
+import matplotlib
+import os.path
+import pandas as pd
+from pathlib import Path
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -96,8 +101,7 @@ radar_name = 'msm'
 #%%
 for indDay, dayEu in enumerate(Eurec4aDays):
 
-    # creating output path for figures
-    Path(pathFig+'/'+dayEu+'/').mkdir(parents=True, exist_ok=True)
+
 
     # extracting strings for yy, dd, mm
     yy              = str(dayEu)[0:4]
@@ -109,15 +113,22 @@ for indDay, dayEu in enumerate(Eurec4aDays):
     dateRadar       = yy[0:2]+mm+dd #'200204'
     dateReverse     = yy+mm+dd      #'20200204'
 
+    # creating output path for figures
+    Path(pathFig+'/'+dateReverse+'/').mkdir(parents=True, exist_ok=True)
+
     pathRadar          = pathFolderTree+'/corrected_data/'+yy+'/'+mm+'/'+dd+'/'
     radarFileList      = np.sort(glob.glob(pathRadar+date+'_*msm94_msm_ZEN_corrected.nc'))
 
-
+    '''
+    Diciamo che dato X dataarray con coordinate tempo, altezza. 
+    la funzione restituisce un dataarray di Nan, con coordinate tempo fissa di '3s' (ma che magari fa scopa con la periodicita' di X) 
+                                                                                     e coordinata altezza uguale a quella di X. Chiameremo questa variabile VUOTO
+    '''
     for iHour, filename in enumerate(radarFileList):
 
         MeanDopVelDataset  = xr.open_dataset(filename)
+        
         # read hour from the selected files
-
         n = len(pathRadar)
         hh                 = filename[n+9:n+11] #19012020_00msm94_msm_ZEN_corrected
         print('plotting hh ', hh)
@@ -125,17 +136,31 @@ for indDay, dayEu in enumerate(Eurec4aDays):
         datetimeRadar      = pd.to_datetime(MeanDopVelDataset['time'].values)
         rangeRadar         = MeanDopVelDataset['range'].values
         Vd[Vd == -999.]    = np.nan
-        Ze                 = MeanDopVelDataset['ze'].values
-        Ze[Ze == -999.]    = np.nan
+        Ze                 = MeanDopVelDataset['ze']
+        #Ze[Ze == -999.]    = np.nan
         ZeLog              = 10.*np.log10(Ze)
         Sw                 = MeanDopVelDataset['sw'].values
-        Sw[Sw == -999.]    = np.nan
+        #Sw[Sw == -999.]    = np.nan
         Sk                 = MeanDopVelDataset['skew'].values
-        Sk[Sk == -999.]    = np.nan
+        #Sk[Sk == -999.]    = np.nan
         timeLocal          = datetimeRadar-timedelta(hours=4)
-
+        
+        timegrid = pd.date_range(timeLocal[0],timeLocal[-1],freq='3s')
+        a = np.zeros((len(timegrid),len(rangeRadar)))
+        a.fill(np.nan)
+        Ze_resampled = xr.DataArray(a, coords=[timegrid, rangeRadar], dims=["time", "range"])
+        Vd_resampled = xr.DataArray(a, coords=[timegrid, rangeRadar], dims=["time", "range"])
+        Sw_resampled = xr.DataArray(a, coords=[timegrid, rangeRadar], dims=["time", "range"])
+        Sk_resampled = xr.DataArray(a, coords=[timegrid, rangeRadar], dims=["time", "range"])
+        
+        for indTime, timeSel in enumerate(timeLocal):
+            indFound = np.where(timegrid == timeSel)[0][0]
+            print(indFound)
+        
+        strasuka
+        
         # set here the variable you want to plot
-        varStringArr = ['Vd', 'Ze', 'Sw', 'Sk', ]
+        varStringArr = ['Ze', 'Vd', 'Sw', 'Sk' ]
 
 
         for ivar in range(len(varStringArr)):
@@ -149,10 +174,14 @@ for indDay, dayEu in enumerate(Eurec4aDays):
                 step = 1.
                 colors =['#4f8c9d', '#1c4c5e', '#8ae1f9', '#8f0f1b', '#e0bfb4', '#754643', '#ef7e58', '#ff1c5d']
                 cmap, ticks, norm, bounds =  f_defineSingleColorPalette(colors, mincm, maxcm, step)
-                var = ZeLog
                 cbarstr = 'Ze [dBz]'
                 strTitle = 'Reflectivity : '+dd+'.'+mm+'.'+yy
-
+                ze_df = pd.DataFrame(ZeLog, index=timeLocal, columns=rangeRadar)
+                #ze_res_df = ze_df.asfreq('3s')
+                ze_res_df = ze_df.data.resample('3s').asfreq()
+                var = ze_res_df.values
+                timeResampled =  pd.to_datetime(ze_res_df.index)
+                
             # settings for mean Doppler velocity
             elif varString == 'Vd':
                 mincm = -4.
@@ -162,9 +191,13 @@ for indDay, dayEu in enumerate(Eurec4aDays):
                 colorsLower = ["#4553c2", "#2b19d9", "#d0d2f0", "#42455e", "#66abf9"]# grigio: 8c8fab
                 colorsUpper = ["#fd2c3b", "#59413f", "#fdc7cc", "#8f323c", "#e66c4c", "#ae8788"] #MDV
                 cmap, ticks, norm, bounds =  f_defineDoubleColorPalette(colorsLower, colorsUpper, mincm, maxcm, step, thrs)
-                var = Vd
+                vd_df = pd.DataFrame(Vd, index=timeLocal, columns=rangeRadar)
                 cbarstr = 'Vd [$ms^{-1}$]'
                 strTitle = 'Mean Doppler velocity : '+dd+'.'+mm+'.'+yy
+                timegrid = pd.date_range(timeLocal[0],timeLocal[-1],freq='3s')
+                vd_res_df = vd_df.reindex(timegrid)
+                var = vd_res_df.values
+                timeResampled =  pd.to_datetime(vd_res_df.index)
 
             # settings for Spectral width
             elif varString == 'Sw':
@@ -174,10 +207,14 @@ for indDay, dayEu in enumerate(Eurec4aDays):
                 #colors = ["#72e5ef", "#460942", "#4dc172", "#cd71b5", "#274c56", "#91ec17", "#b00bd9", "#abc177"]
                 colors = [ "#e6bcbe", "#51443f", "#f17a74", "#e81659", "#cdd2df", "#124ac1"]
                 cmap, ticks, norm, bounds =  f_defineSingleColorPalette(colors, mincm, maxcm, step)
-                var = Sw
+                sw_df = pd.DataFrame(Sw, index=timeLocal, columns=rangeRadar)
                 cbarstr = 'Sw [$ms^{-1}$]'
                 strTitle = 'Spectral width : '+dd+'.'+mm+'.'+yy
-
+                timegrid = pd.date_range(timeLocal[0],timeLocal[-1],freq='3s')
+                sw_res_df = sw_df.reindex(timegrid)
+                var = sw_res_df.values
+                timeResampled =  pd.to_datetime(sw_res_df.index)
+                
             # settings for skewness
             elif varString == 'Sk':
                 mincm = -2.
@@ -187,59 +224,61 @@ for indDay, dayEu in enumerate(Eurec4aDays):
                 colorsLower = ["#082742", "#c5d5f0", "#3693f2", "#555f6e"] # grigio: 8c8fab
                 colorsUpper = ["#5f3e3f", "#ddc0bd", "#fa2e55", "#ee7b85", "#851e39", "#e518a9"]
                 cmap, ticks, norm, bounds =  f_defineDoubleColorPalette(colorsLower, colorsUpper, mincm, maxcm, step, thrs)
-                var = Sk
+                sk_df = pd.DataFrame(Sk, index=timeLocal, columns=rangeRadar)
                 cbarstr = '$Sk$ []'
                 strTitle = 'Skewness : '+dd+'.'+mm+'.'+yy
+                timegrid = pd.date_range(timeLocal[0],timeLocal[-1],freq='3s')
+                sk_res_df = sk_df.reindex(timegrid)
+                var = sk_res_df.values
+                timeResampled =  pd.to_datetime(sk_res_df.index)
+            
+#%%
+timeStartDay = timeLocal[0]
+timeEndDay   = timeLocal[-1]
+hmin         = 0.
+hmax=2500.
+ystring = cbarstr
+labelsizeaxes   = 16
+fontSizeTitle = 16
+fontSizeX = 16
+fontSizeY = 16
+cbarAspect = 10
+fontSizeCbar = 16
+yyPlot = datetimeRadar[0].year
+mmPlot = datetimeRadar[0].month
+ddPlot = datetimeRadar[0].day
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12,6))
+rcParams['font.sans-serif'] = ['Tahoma']
+matplotlib.rcParams['savefig.dpi'] = 100
+plt.gcf().subplots_adjust(bottom=0.15)
+fig.tight_layout()
+ax = plt.subplot(1,1,1)
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.get_xaxis().tick_bottom()
+ax.get_yaxis().tick_left()
+matplotlib.rc('xtick', labelsize=labelsizeaxes)  # sets dimension of ticks in the plots
+matplotlib.rc('ytick', labelsize=labelsizeaxes)  # sets dimension of ticks in the plots
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+ax.xaxis.set_minor_formatter(mdates.DateFormatter("%H:%M"))
+ax.xaxis_date()
 
+cax = ax.pcolormesh(timeResampled, rangeRadar, var.transpose(), vmin=mincm, vmax=maxcm, cmap=cmap)
+ax.set_ylim(hmin,hmax)                                               # limits of the y-axesn  cmap=plt.cm.get_cmap("viridis", 256)
+ax.set_xlim(timeStartDay, timeEndDay)                                 # limits of the x-axes
+ax.set_title(strTitle, fontsize=fontSizeTitle, loc='left')
+ax.set_xlabel("Local Time (UTC - 4h) [hh:mm]", fontsize=fontSizeX)
+ax.set_ylabel("Height [$m$]", fontsize=fontSizeY)
+cbar = fig.colorbar(cax, orientation='vertical', aspect=cbarAspect)
+cbar.set_label(label=cbarstr, size=fontSizeCbar)
+cbar.ax.tick_params(labelsize=labelsizeaxes)
+# Turn on the frame for the twin axis, but then hide all
+# but the bottom spine
 
-            timeStartDay = timeLocal[0]
-            timeEndDay   = timeLocal[-1]
-            hmin         = 0.
-            hmax=2500.
-            ystring = cbarstr
-            labelsizeaxes   = 16
-            fontSizeTitle = 16
-            fontSizeX = 16
-            fontSizeY = 16
-            cbarAspect = 10
-            fontSizeCbar = 16
-            yyPlot = datetimeRadar[0].year
-            mmPlot = datetimeRadar[0].month
-            ddPlot = datetimeRadar[0].day
-            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12,6))
-            rcParams['font.sans-serif'] = ['Tahoma']
-            matplotlib.rcParams['savefig.dpi'] = 100
-            plt.gcf().subplots_adjust(bottom=0.15)
-            fig.tight_layout()
-            ax = plt.subplot(1,1,1)
-            ax.spines["top"].set_visible(False)
-            ax.spines["right"].set_visible(False)
-            ax.get_xaxis().tick_bottom()
-            ax.get_yaxis().tick_left()
-            matplotlib.rc('xtick', labelsize=labelsizeaxes)  # sets dimension of ticks in the plots
-            matplotlib.rc('ytick', labelsize=labelsizeaxes)  # sets dimension of ticks in the plots
-            ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
-            ax.xaxis.set_minor_formatter(mdates.DateFormatter("%H:%M"))
-            ax.xaxis_date()
-            ax.axvline(x=pd.to_datetime(datetime(yyPlot,mmPlot,ddPlot,6,30,0,0)), color='black',linewidth=4, linestyle=':')
-            ax.axvline(x=pd.to_datetime(datetime(yyPlot,mmPlot,ddPlot,19,15,0,0)), color='black', linewidth=4, linestyle=':')
+fig.tight_layout()
+plt.savefig(pathFig+'/'+dateReverse+'/'+yy+mm+dd+'_'+hh+'_'+radar_name+'_'+varString+'_quicklooks.png', format='png', bbox_inches='tight')
 
-            cax = ax.pcolormesh(timeLocal, rangeRadar, var.transpose(), vmin=mincm, vmax=maxcm, cmap=cmap)
-            ax.set_ylim(hmin,hmax)                                               # limits of the y-axesn  cmap=plt.cm.get_cmap("viridis", 256)
-            ax.set_xlim(timeStartDay, timeEndDay)                                 # limits of the x-axes
-            ax.set_title(strTitle, fontsize=fontSizeTitle, loc='left')
-            ax.set_xlabel("Local Time (UTC - 4h) [hh:mm]", fontsize=fontSizeX)
-            ax.set_ylabel("Height [$m$]", fontsize=fontSizeY)
-            cbar = fig.colorbar(cax, orientation='vertical', aspect=cbarAspect)
-            cbar.set_label(label=cbarstr, size=fontSizeCbar)
-            cbar.ax.tick_params(labelsize=labelsizeaxes)
-            # Turn on the frame for the twin axis, but then hide all
-            # but the bottom spine
-
-            fig.tight_layout()
-            plt.savefig(pathFig+'/'+dayEu+'/'+yy+mm+dd+'_'+hh+'_'+radar_name+'_'+varString+'_quicklooks.png', format='png', bbox_inches='tight')
-
-
+#%%
         # -------------------------------------------------------------------
         # --- NEXT PLOT -----------------------------------------------------
         # -------------------------------------------------------------------
@@ -253,6 +292,13 @@ for indDay, dayEu in enumerate(Eurec4aDays):
         data13 = np.ma.masked_invalid(data13)
         data14 = MeanDopVelDataset['pa']
         data14 = np.ma.masked_invalid(data14)
+        fs = 14.
+        time_dd = yy+'-'+mm+'-'+dd
+        t_plot = timeLocal
+        data11 = MeanDopVelDataset['tb']
+        data11 = np.ma.masked_invalid(data11)
+        time1 = timeStartDay
+        time2 =  timeEndDay
         fig, axes = plt.subplots(figsize=[12.0, 15.0], nrows=5, sharex=True)
         # ---------------------------------------------------------------------
         axes[0].plot(t_plot, data10, linestyle='None', marker='o', markersize=3., mec='b', mfc='b')
@@ -299,5 +345,5 @@ for indDay, dayEu in enumerate(Eurec4aDays):
         print('met data overview quicklook produced')
 
         # -----------------------------------------------------------------------
-        plt.savefig(pathFig+'/'+dayEu+'/'+yy+mm+dd+'_'+hh+'_'+radar_name+'_metstation.png')
+        plt.savefig(pathFig+'/'+dateReverse+'/'+yy+mm+dd+'_'+hh+'_'+radar_name+'_metstation.png')
         print('------------ quicklook done ------------')
